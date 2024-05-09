63b8b01e16415ae72eb18ca8c3f77db4d8292681
Beatriz Araujo <beatriz.a.0702@gmail.com>
2024-05-09 11:16:12 -0300

    feat: adds logic for input and output

--- src/org/contabanco/ContaBanco.java
+++ src/org/contabanco/ContaBanco.java
@@ -1,8 +1,24 @@
 package org.contabanco;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaBanco {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Por favor, digite o número da sua conta: \t");
+        int accountNumber = scanner.nextInt();
+        System.out.print("Por favor, digite o número da sua agência: \t");
+        String agencyNumber = scanner.next();
+        System.out.print("Por favor, digite o seu nome: \t");
+        String name = scanner.next();
+        System.out.print("Por favor, digite o saldo: \t");
+        double amount = scanner.nextDouble();
+
+        System.out.println("\nProcessando suas informações \n");
+
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencyNumber + ", conta nº" + accountNumber + " e seu saldo R$" + amount + " já está disponível para saque.");
+        scanner.close();
     }
 }
